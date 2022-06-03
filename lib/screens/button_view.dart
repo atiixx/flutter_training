f2c90139beb80674afe6425b8ec754ddd716a5a0
j.hauenstein <2022265@stud.hs-mannheim.de>
2022-06-03 12:21:01 +0200

    Buttonstyles ausprobieren

--- lib/screens/button_view.dart
+++ lib/screens/button_view.dart
@@ -2,37 +2,58 @@ import 'package:flutter/material.dart';
 
 import 'colorscroll_view.dart';
 
-class ButtonView extends StatelessWidget {
+class ButtonView extends StatefulWidget {
   const ButtonView({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _buttonviewstate();
+  }
+}
+
+class _buttonviewstate extends State<ButtonView> {
+  bool pressed = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.lightBlue,
       appBar: AppBar(
         title: Text("Spaß mit Buttons"),
-    ),
+      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget> [
-ElevatedButton(onPressed: () {
-  Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => const ColorScrollView()),
-  );
-
-}, child: const Text("Zur Scrollview")),
-
-
-          ]
-
-          ),
-
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  pressed = !pressed;
+                });
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ColorScrollView()),
+                );
+              },
+              child: const Text("Zur Scrollview"),
+              style: pressed
+                  ? ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.lightGreen), textStyle: MaterialStateProperty.all(Colors.white))
+                  : ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.green)),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.green)),
+              child: const Text("Test2"),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.green)),
+              child: const Text("Test3"),
+            ),
+          ],
         ),
-      );
+      ),
+    );
   }
-
-
-
-
 }
